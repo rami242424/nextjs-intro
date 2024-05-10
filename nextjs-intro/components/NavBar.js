1fc13517012b947a10bd93ef7bda6d9aeaee3083
Rami <138556019+rami242424@users.noreply.github.com>
2024-05-10 20:42:21 +0900

    #6.5 Styles JSX - style jsx global로 독립적으로 css 꾸며주기

--- nextjs-intro/components/NavBar.js
+++ nextjs-intro/components/NavBar.js
@@ -1,24 +1,24 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import styles from "./NavBar.module.css";
 
 export default function NavBar(){
     const router = useRouter();
     return(
         <nav>
-            <Link href="/" className={`${styles.link} ${
-                router.pathname === "/" ? styles.active : ""
-            }`}
-            >
+            <Link href="/" legacyBehavior>
                 Home
             </Link>
-            <Link href="/about" className={[
-                styles.link,
-                router.pathname === "/about" ? styles.active : "",
-            ].join(" ")}
-            >
+            <Link href="/about" legacyBehavior>
                 About
             </Link>
+            <style jsx global>{`
+                nav {
+                    background-color: tomato;
+                }
+                a {
+                    text-decoration: none;
+                }
+            `}</style>
         </nav>
     )
 }
