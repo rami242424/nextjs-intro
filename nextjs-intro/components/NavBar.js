bb32f1a1914c8d07da1e4090e68ed9f11f18a224
Rami <138556019+rami242424@users.noreply.github.com>
2024-05-10 20:30:52 +0900

    6.4 CSS Modules - 방법2(module.css)

--- nextjs-intro/components/NavBar.js
+++ nextjs-intro/components/NavBar.js
@@ -6,8 +6,19 @@ export default function NavBar(){
     const router = useRouter();
     return(
         <nav>
-            <Link href="/" className={router.pathname === "/" ? styles.active : ""}>Home</Link>
-            <Link href="/about" className={router.pathname === "/about" ? styles.active : ""}>About</Link>
+            <Link href="/" className={`${styles.link} ${
+                router.pathname === "/" ? styles.active : ""
+            }`}
+            >
+                Home
+            </Link>
+            <Link href="/about" className={[
+                styles.link,
+                router.pathname === "/about" ? styles.active : "",
+            ].join(" ")}
+            >
+                About
+            </Link>
         </nav>
     )
 }
