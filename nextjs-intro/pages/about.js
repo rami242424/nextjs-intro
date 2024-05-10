13f0f2fc0e370f0f200f58962ec0798a36dae4a7
Rami <138556019+rami242424@users.noreply.github.com>
2024-05-10 20:17:20 +0900

    6.3 Routing

---
+++ nextjs-intro/pages/about.js
@@ -0,0 +1,11 @@
+import NavBar from "@/components/NavBar"
+
+export default function AboutUs(){
+    return(
+        <div>
+            <NavBar/>
+            <h1>"I am in a AboutUs"</h1>
+        </div>
+        
+    );
+}
